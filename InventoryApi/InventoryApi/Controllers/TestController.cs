c058bec36b4b80a41ecf08f5695aee2ee39cb4cd
ChamorroSamuel <102174595+ChamorroSamuel@users.noreply.github.com>
2025-04-23 21:29:24 -0500

    Verify initial project setup
    Tested and confirmed that the initial InventoryApi project runs successfully with no critical errors. Basic endpoint or server response is working.

---
+++ InventoryApi/InventoryApi/Controllers/TestController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InventoryApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() 
+            => Ok(new { message = "¡Swagger está funcionando!" });
+    }
+}
